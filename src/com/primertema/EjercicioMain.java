bab6002a61ac74884d11170923259e4e4ee3916a
raxfor <jhonalexslb@gmail.com>
2022-10-12 21:18:03 -0500

    Ejercicio tema 1

---
+++ src/com/primertema/EjercicioMain.java
@@ -0,0 +1,28 @@
+package com.primertema;
+
+public class EjercicioMain {
+    public static void main(String[] args) {
+        byte edad = 18;
+        System.out.println("la edad es " + edad + " es de tipo byte");
+        short numero = 34;
+        System.out.println("El numero " + numero + " es de tipo short");
+        int numeroEntero = 50;
+        System.out.println("El numero " + numeroEntero + " es de tipo int");
+        long numeroBig = 456;
+        System.out.println("El numero " + numeroBig + " es de tipo long");
+        float numeroFlotante = 345.434f;
+        System.out.println("El numero " + numeroFlotante + " es de tipo float");
+        double numeroDecimal = 45664.45454;
+        System.out.println("El numero" + numeroDecimal + " es de tipo double");
+        boolean verdadero = true;
+        System.out.println("La variable es " + verdadero + " es de tipo boolean");
+        boolean falso = false;
+        System.out.println("La variable es " + falso + " es de tipo boolean");
+        char letra = 'a';
+        System.out.println("La letra es " + letra + " es de tipo char");
+        String palabra = "Hola";
+        System.out.println("La palabra es " + palabra + " es de tipo String");
+
+
+    }
+}
